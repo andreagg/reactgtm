300a93cb91818a5d9d7e5a0dd27fbeaf42ad5782
andrea.grande <gandrea701@gmail.com>
2024-09-27 18:04:12 +0200

    yes gh pages

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "dist", // La cartella di output del bundle
-  },
-});
