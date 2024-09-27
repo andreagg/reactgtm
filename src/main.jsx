3151c6e26e5d7aeac66673ce82269d6b841022b1
andrea.grande <gandrea701@gmail.com>
2024-09-27 18:01:19 +0200

    ok

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+// Esportiamo una funzione di inizializzazione per permettere a GTM di avviare l'app
+window.ReactApp = {
+  init: (selector) => {
+    const rootElement = document.querySelector(selector);
+    if (rootElement) {
+      ReactDOM.createRoot(rootElement).render(
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      );
+    }
+  },
+};
